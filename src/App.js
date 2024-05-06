3744b2b72e3d53924f9b6ee004269b5fcd625f79
HaTaeMin <gkxoals33@gmail.com>
2024-05-06 17:12:57 +0900

    라우터설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
-import "./assets/css/tStyle.scss"
+import "./assets/css/tStyle.scss";
+import { Outlet, Route, Routes } from "react-router-dom";
+import Home from "./pages/MainPage/Home";
+import Header from "./layouts/Header/Header";
+import Footer from "./layouts/Footer/Footer";
+import Login from "./pages/LoginPage/Login";
+import Register from "./pages/RegisterPage/Register";
+import MateList from "./pages/GoingWithPage/MateList";
+import RestaurantList from "./pages/RestaurantPage/RestaurantList";
+import RestaurantView from "./pages/RestaurantPage/RestaurantView";
+import ReviewAdd from "./pages/ReviewPage/ReviewAdd";
+import Review from "./pages/ReviewPage/Review";
+import MeetingList from "./pages/MeetingPage/MeetingList";
+import MeetingAdd from "./pages/MeetingPage/MeetingAdd";
+import MeetingView from "./pages/MeetingPage/MeetingView";
+import Account from "./pages/AccountPage/Account";
+import AccountEdit from "./pages/AccountPage/AccountEdit";
+import AccountPwdEdit from "./pages/AccountPage/AccountPwdEdit";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
 
 function App() {
   return (
-   <div >
-     test
-   </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/mate" element={<MateList />}></Route>
+          <Route path="/mate/:cateId" element={<RestaurantList />}></Route>
+          <Route
+            path="/mate/:cateId/restaurants/:rtId"
+            element={<RestaurantView />}
+          ></Route>
+          <Route
+            path="/mate/restaurants/:rtId/review-post/new"
+            element={<ReviewAdd />}
+          ></Route>
+          <Route
+            path="/mate/restaurants/:rtId/review-post/:rpId"
+            element={<Review />}
+          ></Route>
+          <Route path="/meet-posts" element={<MeetingList />}></Route>
+          <Route path="/meet-posts/new" element={<MeetingAdd />}></Route>
+          <Route path="/meet-posts/:mpId" element={<MeetingView />}></Route>
+          <Route path="/account/:userId" element={<Account />}></Route>
+          <Route path="/account/:userId/edit" element={<AccountEdit />}></Route>
+          <Route
+            path="/account/:userId/pwd-edit"
+            element={<AccountPwdEdit />}
+          ></Route>
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- src/pages/MainPage/Home.js
+++ src/pages/MainPage/Home.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Home(props) {
-    return (
-        <div></div>
-    );
+  return <div>tes</div>;
 }
 
 export default Home;
