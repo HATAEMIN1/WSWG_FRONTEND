39a274b6bc4bf92940c96c6063559f8f19c788f2
Chloe Yeo <yeochloe01@gmail.com>
2024-05-07 18:40:44 +0900

    레이아웃 기본

--- src/store/thunkFunctions.js
+++ src/store/thunkFunctions.js
@@ -1,1 +1,65 @@
 //createAsyncThunk
+
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axiosInstance from "../utils/axios";
+
+export const registerUser = createAsyncThunk(
+    "user/registerUser",
+    async (body, thunkAPI) => {
+        try {
+            const response = await axiosInstance.post(`/user/register`, body);
+            console.log("thunkapi 회원가입");
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            return thunkAPI.rejectWithValue(
+                error.response.data || error.message
+            );
+        }
+    }
+);
+
+export const loginUser = createAsyncThunk(
+    "user/loginUser",
+    async (body, thunkAPI) => {
+        try {
+            const res = await axiosInstance.post("/user/login", body);
+            return res.data;
+        } catch (error) {
+            console.log(error);
+            return thunkAPI.rejectWithValue(
+                error.response.data || error.message
+            );
+        }
+    }
+);
+
+export const authUser = createAsyncThunk(
+    "user/authUser",
+    async (_, thunkAPI) => {
+        try {
+            const response = await axiosInstance.get(`/user/auth`);
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            return thunkAPI.rejectWithValue(
+                error.response.data || error.message
+            );
+        }
+    }
+);
+
+export const logoutUser = createAsyncThunk(
+    "user/logoutUser",
+    async (_, thunkAPI) => {
+        try {
+            const response = await axiosInstance.post(`/user/logout`);
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            return thunkAPI.rejectWithValue(
+                error.response.data || error.message
+            );
+        }
+    }
+);
