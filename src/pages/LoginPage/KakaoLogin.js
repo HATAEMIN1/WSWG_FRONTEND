26c780ea13e5e83b3658777a532afb7c7c7afcb0
Chloe Yeo <yeochloe01@gmail.com>
2024-05-08 18:32:23 +0900

    일단 카카오로그인버튼과 네이버로그인버튼 넣음

---
+++ src/pages/LoginPage/KakaoLogin.js
@@ -0,0 +1,9 @@
+const KakaoLogin = () => {
+    return (
+        <button className="w-[400px] h-6 px-2.5 py-[5px] mb-10 rounded-[12px] block">
+            <img src="./images/kakao_login_large_wide.png" alt="kakao login" />
+        </button>
+    );
+};
+
+export default KakaoLogin;
---
+++ src/pages/LoginPage/NaverLogin.js
@@ -0,0 +1,9 @@
+const NaverLogin = () => {
+    return (
+        <button className="w-[400px] px-2.5 py-[5px] mb-4 rounded-[12px] block">
+            <img src="./images/naver_login.png" alt="kakao login" />
+        </button>
+    );
+};
+
+export default NaverLogin;
