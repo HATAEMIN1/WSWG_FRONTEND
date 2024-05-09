e05e07f08afda7ff5f741349527ecbcca11cacd2
HaTaeMin <gkxoals33@gmail.com>
2024-05-09 17:51:08 +0900

    axios준비

--- src/pages/GoingWithPage/MateList.js
+++ src/pages/GoingWithPage/MateList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SectionWarp from "../../components/Layout/Section";
+import { SectionWarp } from "../../components/Layout/Section";
 import { Link } from "react-router-dom";
 function MateList(props) {
     const category = [
--- src/pages/RestaurantPage/RestaurantList.js
+++ src/pages/RestaurantPage/RestaurantList.js
@@ -1,6 +1,8 @@
-import React from "react";
-import SectionWarp from "../../components/Layout/Section";
+import React, { useEffect } from "react";
+import { SectionWarp } from "../../components/Layout/Section";
 import { useParams } from "react-router-dom";
+import Title from "../../components/Layout/Title";
+import axiosInstance from "../../utils/axios";
 
 function RestaurantList(props) {
     const category = [
@@ -43,16 +45,57 @@ function RestaurantList(props) {
     ];
     const { cateId } = useParams();
     const selectedCategory = category.find((item) => item.cateId === cateId);
+
+    useEffect(() => {
+        async function restaurantInfo() {
+            try {
+                const res = await axiosInstance.get("/:cateId", {
+                    params: cateId,
+                });
+                console.log(res);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        restaurantInfo();
+    }, []);
     return (
         <SectionWarp>
-            <h1>{selectedCategory.name}과 가볼까</h1>
+            <Title memTitle={false}>{selectedCategory.name}과 가볼까</Title>
             <div>
                 <select>
-                    <option> 광역시도</option>
+                    <option selected disabled>
+                        광역시도
+                    </option>
+                    <option>서울특별시</option>
                 </select>
-                <select>시도군</select>
-                <select>읍면동</select>
-                <button>지역 변경</button>
+                <select>
+                    <option selected disabled>
+                        시군구
+                    </option>
+                </select>
+                <select>
+                    <option selected disabled>
+                        읍면동
+                    </option>
+                </select>
+                <button className="border rounded-md">지역 변경</button>
+            </div>
+            <div className="grid grid-cols-2 w-full border">
+                <div className="flex gap-7">
+                    <div>
+                        <img
+                            className="w-[186.60px] h-40"
+                            src="/images/mate_family.png"
+                            alt=""
+                        />
+                    </div>
+                    <div>
+                        <h3>호족반 도산공원점</h3>
+                        <p>한식</p>
+                        <p>평점</p>
+                    </div>
+                </div>
             </div>
         </SectionWarp>
     );
