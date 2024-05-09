4f901dc4354f7d862e6fee11c7c3d0b9830fa66e
Chloe Yeo <yeochloe01@gmail.com>
2024-05-09 10:14:14 +0900

    카카오에서 인가코드 받아오기

--- src/pages/LoginPage/KakaoLogin.js
+++ src/pages/LoginPage/KakaoLogin.js
@@ -1,6 +1,16 @@
 const KakaoLogin = () => {
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize
+?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}
+&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}
+&response_type=code`;
+    const LoginWithKakao = () => {
+        window.location.href = KAKAO_AUTH_URL;
+    };
     return (
-        <button className="w-[400px] h-6 px-2.5 py-[5px] mb-10 rounded-[12px] block">
+        <button
+            onClick={LoginWithKakao}
+            className="w-[400px] h-6 px-2.5 py-[5px] mb-10 rounded-[12px] block"
+        >
             <img src="./images/kakao_login_large_wide.png" alt="kakao login" />
         </button>
     );
