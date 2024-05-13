a7fb06596936f3936fcb2e3089d5824736d8daf4
Chloe Yeo <yeochloe01@gmail.com>
2024-05-13 18:38:07 +0900

    updated account page내 정보 페이지 업데이트

--- src/pages/AccountPage/Account.js
+++ src/pages/AccountPage/Account.js
@@ -1,28 +1,38 @@
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 
 function Account() {
     const isAuth = useSelector((state) => state.user.isAuth);
     const userData = useSelector((state) => state.user.userData.user);
-    const navigate = useNavigate();
+
     console.log("isAuth from account", isAuth);
-    if (isAuth) {
-        console.log("state.user.userData.user", userData);
-        return (
-            <div>
-                <h2>User Profile</h2>
-                {userData && (
+
+    return (
+        <>
+            <div
+                className="w-[100%] h-[100px] mt-4 flex-col justify-start items-center inline-flex font-normal text-zinc-800"
+                style={{ fontFamily: "TTHakgyoansimMonggeulmonggeulR" }}
+            >
+                <div className="text-center text-5xl">어까</div>
+                <div className="text-center text-3xl">
+                    나는 어디까지 가봤을까?
+                </div>
+            </div>
+            <div className="flex justify-center">
+                {isAuth && (
                     <div>
-                        <p>Email: {userData.email}</p>
-                        <p>Name: {userData.name}</p>
-                        <p>Id: {userData.id}</p>
+                        <h2>User Profile</h2>
+                        {userData && (
+                            <div>
+                                <p>Email: {userData.email}</p>
+                                <p>Name: {userData.name}</p>
+                                <p>Id: {userData.id}</p>
+                            </div>
+                        )}
                     </div>
                 )}
             </div>
-        );
-    } else {
-        navigate("/login");
-    }
+        </>
+    );
 }
 
 export default Account;
