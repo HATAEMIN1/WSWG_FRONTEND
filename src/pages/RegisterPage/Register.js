3ebfc57ea00fb7c63015cd85a80cd752217e76a5
Chloe Yeo <yeochloe01@gmail.com>
2024-05-20 17:43:39 +0900

    updated icons in register page

--- src/pages/RegisterPage/Register.js
+++ src/pages/RegisterPage/Register.js
@@ -88,9 +88,9 @@ function Register() {
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <div className="emailWrap flex justify-between mt-5 mb-5">
                             <div className="w-10 h-10 relative">
-                                <div className="w-[25.20px] h-[25.20px] left-[7px] top-[7px] absolute">
+                                <div className="w-[35px] h-[35px] left-[7px] top-[2px] absolute">
                                     <img
-                                        src="./images/icon_Email.svg"
+                                        src="./images/iconMail.png"
                                         alt="email icon"
                                     />
                                 </div>
@@ -114,9 +114,9 @@ function Register() {
                         </div>
                         <div className="usernameWrap flex justify-between mb-5">
                             <div className="w-10 h-10 relative">
-                                <div className="w-[25.20px] h-[25.20px] left-[7px] top-[7px] absolute">
+                                <div className="w-[35px] h-[35px] left-[7px] top-[2px] absolute">
                                     <img
-                                        src="./images/icon_Person.svg"
+                                        src="./images/iconPerson.png"
                                         alt="person icon"
                                     />
                                 </div>
@@ -142,10 +142,10 @@ function Register() {
                         </div>
                         <div className="passwordWrap flex justify-between mb-5">
                             <div className="w-10 h-10 relative">
-                                <div className="w-[25.20px] h-[25.20px] left-[7px] top-[7px] absolute">
+                                <div className="w-[35px] h-[35px] left-[7px] top-[2px] absolute">
                                     <img
-                                        src="./images/icon_Person.svg"
-                                        alt="person icon"
+                                        src="./images/iconPwd.png"
+                                        alt="password key icon"
                                     />
                                 </div>
                             </div>
@@ -189,10 +189,10 @@ function Register() {
                         </div>
                         <div className="passwordConfirmWrap flex justify-between mb-5">
                             <div className="w-10 h-10 relative">
-                                <div className="w-[25.20px] h-[25.20px] left-[7px] top-[7px] absolute">
+                                <div className="w-[35px] h-[35px] left-[7px] top-[2px] absolute">
                                     <img
-                                        src="./images/icon_Person.svg"
-                                        alt="person icon"
+                                        src="./images/iconPwdDoubleCheck.png"
+                                        alt="password double check icon"
                                     />
                                 </div>
                             </div>
