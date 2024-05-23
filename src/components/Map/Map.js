559e6863e3fcd471dd1d9561d3842433dc31fe05
HaTaeMin <gkxoals33@gmail.com>
2024-05-23 12:56:25 +0900

    새로고침시 마커 사라지는 문제 해결

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -19,10 +19,14 @@ function Map(props) {
         );
         return selectedMateType ? selectedMateType.cateId : "";
     });
+    const foodtype = useSelector((state) => state.filter.foodType);
     const [geoData, setGeoData] = useState([]);
     const fetchRestaurant = async () => {
         try {
-            const res = await axiosInstance.get(`/restaurants/${cateId}`);
+            const params = { foodtype };
+            const res = await axiosInstance.get(`/restaurants/${cateId}`, {
+                params,
+            });
             console.log(res.data.restaurant);
             setGeoData(res.data.restaurant);
         } catch (e) {
@@ -36,7 +40,9 @@ function Map(props) {
             restaurant.longitude
         ),
     }));
-
+    useEffect(() => {
+        fetchRestaurant();
+    }, []);
     useEffect(() => {
         const mapContainer = document.getElementById("map"), // 지도를 표시할 div
             mapOption = {
@@ -54,22 +60,20 @@ function Map(props) {
 
         for (let i = 0; i < positions.length; i++) {
             // 마커 이미지의 이미지 크기 입니다
-            const imageSize = new kakao.maps.Size(24, 35);
+            let imageSize = new kakao.maps.Size(24, 35);
 
             // 마커 이미지를 생성합니다
-            const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+            let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
             // 마커를 생성합니다
-            const marker = new kakao.maps.Marker({
+            let marker = new kakao.maps.Marker({
                 map: map, // 마커를 표시할 지도
                 position: positions[i].latlng, // 마커를 표시할 위치
                 title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                 image: markerImage, // 마커 이미지
             });
         }
-
-        fetchRestaurant();
-    }, []);
+    }, [geoData]);
 
     return (
         <>
