a96f999e99aad7486540e52da43d7ab3ab679440
HaTaeMin <gkxoals33@gmail.com>
2024-05-31 13:09:12 +0900

    해쉬태그 검색

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 function Header({ ...props }) {
     const [search, setSearch] = useState("");
+    const [tag, setTag] = useState(""); // 새로운 상태 추가
     // const [filename, setFilename] = useState("");
     const isAuth = useSelector((state) => {
         return state.user.isAuth;
@@ -27,11 +28,16 @@ function Header({ ...props }) {
     }
     function handleSearch(e) {
         setSearch(e.target.value);
+        setTag(e.target.value);
     }
     function handleSubmit(e) {
         e.preventDefault();
-        navigate(`/search?q=${search}`);
+        // navigate(`/search?q=${search}`);
+        // setSearch("");
+        const queryString = `?q=${search}&tag=${tag}`; // q와 tag를 함께 URL에 포함
+        navigate(`/search${queryString}`); // 수정된 URL로 이동
         setSearch("");
+        setTag(""); // 검색 후 태그 초기화
     }
     return (
         <>
@@ -67,62 +73,60 @@ function Header({ ...props }) {
                         ></input>
                         <button className="icon iconSearch">검색</button>
                     </form>
-                    
-                        {isAuth ? (
-                            <div className="flex w-auto gap-4 justify-between items-center">
-                                <Link to="/account">
-                                    {oauthLogin ? (
-                                        <div className="w-[50px] h-[50px]">
-                                            <img
-                                                className="rounded-full w-full h-full object-cover"
-                                                src={retrievedImageOauth}
-                                                alt="profileImageFromOauthProfile"
-                                            />
-                                        </div>
-                                    ) : (
-                                        <>
-                                            {retrievedImage !==
-                                            "noimage.jpg" ? (
-                                                <div className="w-[50px] h-[50px]">
-                                                    <img
-                                                        className="rounded-full w-full h-full object-cover"
-                                                        src={
-                                                            process.env
-                                                                .REACT_APP_NODE_SERVER_UPLOAD_URL +
-                                                            retrievedImage
-                                                        }
-                                                        alt="profileImage"
-                                                    />
-                                                </div>
-                                            ) : (
+
+                    {isAuth ? (
+                        <div className="flex w-auto gap-4 justify-between items-center">
+                            <Link to="/account">
+                                {oauthLogin ? (
+                                    <div className="w-[50px] h-[50px]">
+                                        <img
+                                            className="rounded-full w-full h-full object-cover"
+                                            src={retrievedImageOauth}
+                                            alt="profileImageFromOauthProfile"
+                                        />
+                                    </div>
+                                ) : (
+                                    <>
+                                        {retrievedImage !== "noimage.jpg" ? (
+                                            <div className="w-[50px] h-[50px]">
                                                 <img
-                                                    className="w-full h-full object-cover"
-                                                    src="/images/profileDefault.png"
-                                                    alt="defaultPic"
+                                                    className="rounded-full w-full h-full object-cover"
+                                                    src={
+                                                        process.env
+                                                            .REACT_APP_NODE_SERVER_UPLOAD_URL +
+                                                        retrievedImage
+                                                    }
+                                                    alt="profileImage"
                                                 />
-                                            )}
-                                        </>
-                                    )}
-                                </Link>
-                                <Link className="flex-none" onClick={handleLogout}>
-                                    <img
-                                        src={`${process.env.PUBLIC_URL}/images/iconLogout.png`}
-                                        alt="logout"
-                                    />
-                                </Link>
-                            </div>
-                        ) : (
-                            <div className="flex-none userProfile pt-1 md:pt-0">
+                                            </div>
+                                        ) : (
+                                            <img
+                                                className="w-full h-full object-cover"
+                                                src="/images/profileDefault.png"
+                                                alt="defaultPic"
+                                            />
+                                        )}
+                                    </>
+                                )}
+                            </Link>
+                            <Link className="flex-none" onClick={handleLogout}>
+                                <img
+                                    src={`${process.env.PUBLIC_URL}/images/iconLogout.png`}
+                                    alt="logout"
+                                />
+                            </Link>
+                        </div>
+                    ) : (
+                        <div className="flex-none userProfile pt-1 md:pt-0">
                             <Link to="/login">
                                 <img
                                     src="/images/profileDefault.png"
                                     alt="defaultPic"
                                 />
                             </Link>
-                            </div>
-                        )}
-                    </div>
-                
+                        </div>
+                    )}
+                </div>
             </header>
         </>
     );
